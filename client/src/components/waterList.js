4c0043e1425a148c2b55f745d83f24e1c94fad39
allyson_s <allysonsmithstudio@gmail.com>
2022-09-13 16:54:25 -0400

    WaterList component filtering data by waterDate now

--- client/src/components/waterList.js
+++ client/src/components/waterList.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Plant from "./plant";
 
-export default function WaterList(props) {
+export default function WaterList() {
   const [plants, setPlants] = useState([]);
 
   //fetches the records from the database
@@ -24,18 +24,38 @@ export default function WaterList(props) {
     return;
   }, [plants.length]);
 
-  //this method will map out the records in a list
-  function waterList() {
+
+  //map out the plants that are due or overdue for watering
+
+  function waterList(plants) {
+      let filter = plants.filter((plant) => {
+        let today = new Date().getTime();
+        let waterDate = new Date(plant.waterDate).getTime();
+
+        return waterDate <= today;
+      })
+      return filter;
+      
+  }
+  console.log(waterList(plants))
+  //this method will map out all the records in a list
+  /*function waterList() {
     return plants.map((plant) => {
       return <Plant name={plant.name} img={plant.img} key={plant._id} />;
     });
   }
+  */
 
   return (
     <div className="water-list">
       <h2>Water Me!</h2>
       <p>check off to reset water schedule and remove from list</p>
-      <ul className="water-list__plants">{waterList()}</ul>
+      <ul className="water-list__plants">
+        { waterList(plants).map(plant => 
+          <li>
+            <Plant name={plant.name} img={plant.img} key={plant._id}></Plant>
+          </li>) }
+      </ul>
     </div>
   );
 }
