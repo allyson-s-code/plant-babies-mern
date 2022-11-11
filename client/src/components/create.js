adbc1d5969c018a0c1dac2c50af206c405ec6a14
allyson_s <allysonsmithstudio@gmail.com>
2022-11-11 11:29:46 -0500

    Create form is working as expected and add another option is working without validation errors

--- client/src/components/create.js
+++ client/src/components/create.js
@@ -1,14 +1,17 @@
 import React from "react";
-import { useFormik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import TextError from "./textError";
 import { Link } from "react-router-dom";
 
 export default function NewPlant() {
-  async function postData() {
-    // When a post request is sent to the create url, we'll add a new record to the database.
-    const newPlant = { ...formik.values };
+  const onSubmit = async (values, actions) => {
+    alert(JSON.stringify(values, null, 2));
 
-    await fetch("http://localhost:4000/plants/create", {
+    // When a post request is sent to the create url, we'll add a new record to the database.
+    const newPlant = { ...values };
+
+    const response = fetch("http://localhost:4000/plants/create", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -18,198 +21,174 @@ export default function NewPlant() {
       window.alert(error);
       return;
     });
-  }
-
-  const formik = useFormik({
-    initialValues: {
-      name: "",
-      botanicalName: "",
-      img: "https://images.unsplash.com/photo-1581573025746-0ee51aef032a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80",
-      waterFrequency: "",
-      feedFrequency: "",
-      light: "",
-      care: "",
-      waterDate: "",
-      feedDate: "",
-    },
-
-    validationSchema: Yup.object({
-      name: Yup.string().required("Required"),
-      botanicalName: Yup.string().required("Required"),
-      img: Yup.string().url().nullable(),
-      waterFrequency: Yup.number().required("Required").positive().integer(),
-      feedFrequency: Yup.number().positive().nullable(),
-      light: Yup.string().required("Required"),
-      care: Yup.string().required("Required"),
-      waterDate: Yup.string().required("Required"),
-      feedDate: Yup.string().nullable(),
-    }),
-    onSubmit: (values, actions) => {
-      alert(JSON.stringify(values, null, 2));
-      postData(values);
+    if (response) {
       actions.setSubmitting(false);
-      actions.setStatus("sent");
       actions.resetForm();
-    },
+
+      actions.setStatus("sent");
+
+      console.log("your plant baby has been updated:)");
+    }
+  };
+
+  const initialValues = {
+    name: "",
+    botanicalName: "",
+    img: "https://images.unsplash.com/photo-1581573025746-0ee51aef032a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80",
+    waterFrequency: "",
+    feedFrequency: "",
+    light: "",
+    care: "",
+    waterDate: "",
+    feedDate: "",
+  };
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Required"),
+    botanicalName: Yup.string().required("Required"),
+    img: Yup.string().url().nullable(),
+    waterFrequency: Yup.number().required("Required").positive().integer(),
+    feedFrequency: Yup.number().positive().nullable(),
+    light: Yup.string().required("Required"),
+    care: Yup.string().required("Required"),
+    waterDate: Yup.string().required("Required"),
+    feedDate: Yup.string().nullable(),
   });
 
   return (
     <section className="create-section">
-      <h2>Add a New Plant!</h2>
+      <h2>Add Your Plant</h2>
 
-      <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="name">Name:</label>
-        <input
-          id="name"
-          name="name"
-          type="text"
-          placeholder="Spider Plant"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.name}
-        />
-        {formik.touched.name && formik.errors.name ? (
-          <div className="error-msg">{formik.errors.name}</div>
-        ) : null}
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}
+      >
+        {(formik) => {
+          console.log("Formik Props", formik);
+          return (
+            <Form>
+              <label htmlFor="name">Name:</label>
+              <Field
+                id="name"
+                name="name"
+                type="text"
+                placeholder="Spider Plant"
+              />
+              <ErrorMessage name="name" component={TextError} />
 
-        <label htmlFor="botanicalName">Botanical Name:</label>
-        <input
-          id="botanicalName"
-          name="botanicalName"
-          type="text"
-          placeholder="Chlorophytum comosum"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.botanicalName}
-        />
-        {formik.touched.botanicalName && formik.errors.botanicalName ? (
-          <div className="error-msg">{formik.errors.botanicalName}</div>
-        ) : null}
+              <label htmlFor="botanicalName">Botanical Name:</label>
+              <Field
+                id="botanicalName"
+                name="botanicalName"
+                type="text"
+                placeholder="Chlorophytum comosum"
+              />
+              <ErrorMessage name="botanicalName" component={TextError} />
 
-        <label htmlFor="img">
-          Image (url) <span>or keep default-</span>
-        </label>
-        <input
-          id="img"
-          name="img"
-          type="url"
-          placeholder="https://images/unsplash.com/photo-1"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.img}
-        />
-        {formik.touched.img && formik.errors.img ? (
-          <div className="error-msg">{formik.errors.img}</div>
-        ) : null}
+              <label htmlFor="img">
+                Image (url) <span>or leave blank-</span>
+              </label>
+              <Field
+                id="img"
+                name="img"
+                type="url"
+                placeholder="https://images.unsplash.com/photo-1581573025746-0ee51aef032a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80"
+              />
+              <ErrorMessage name="img" component={TextError} />
 
-        <label htmlFor="waterFrequency">Water Frequency (in days):</label>
-        <input
-          id="waterFrequency"
-          name="waterFrequency"
-          type="number"
-          placeholder="7"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.waterFrequency}
-        />
-        {formik.touched.waterFrequency && formik.errors.waterFrequency ? (
-          <div className="error-msg">{formik.errors.waterFrequency}</div>
-        ) : null}
+              <label htmlFor="waterFrequency">Water Frequency (in days):</label>
+              <Field
+                id="waterFrequency"
+                name="waterFrequency"
+                type="number"
+                placeholder="7"
+              />
+              <ErrorMessage name="waterFrequency" component={TextError} />
 
-        <label htmlFor="feedFrequency">Feed Frequency (in days):</label>
-        <input
-          id="feedFrequency"
-          name="feedFrequency"
-          type="number"
-          placeholder="30"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.feedFrequency}
-        />
-        {formik.touched.feedFrequency && formik.errors.feedFrequency ? (
-          <div className="error-msg">{formik.errors.feedFrequency}</div>
-        ) : null}
+              <label htmlFor="feedFrequency">Feed Frequency (in days):</label>
+              <Field
+                id="feedFrequency"
+                name="feedFrequency"
+                type="number"
+                placeholder="30"
+              />
+              <ErrorMessage name="feedFrequency" component={TextError} />
 
-        <label htmlFor="light">Light:</label>
-        <input
-          id="light"
-          name="light"
-          type="text"
-          placeholder="Partial, shade"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.light}
-        />
-        {formik.touched.light && formik.errors.light ? (
-          <div className="error-msg">{formik.errors.light}</div>
-        ) : null}
+              <label htmlFor="light">Light:</label>
+              <Field
+                id="light"
+                name="light"
+                type="text"
+                placeholder="Partial, shade"
+              />
+              <ErrorMessage name="light" component={TextError} />
 
-        <label htmlFor="care">Care:</label>
-        <textarea
-          id="care"
-          name="care"
-          type="text"
-          placeholder="Spider plants are often grown in containers as hanging plants due to the cascading nature of their foliage and their long stems with plantlets. 
+              <label htmlFor="care">Care:</label>
+              <Field
+                as="textarea"
+                id="care"
+                name="care"
+                type="text"
+                placeholder="Spider plants are often grown in containers as hanging plants due to the cascading nature of their foliage and their long stems with plantlets. 
           Regular watering is typically the most time-consuming part of spider plant care. Throughout the growing season (spring to fall) also plan to fertilize regularly. And repot your plant as needed once its roots have outgrown the container. "
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.care}
-          className="care-input"
-        />
-        {formik.touched.care && formik.errors.care ? (
-          <div className="error-msg">{formik.errors.care}</div>
-        ) : null}
+              />
+              <ErrorMessage name="care" component={TextError} />
 
-        <label htmlFor="waterDate">Next Water Date:</label>
-        <input
-          id="waterDate"
-          name="waterDate"
-          type="date"
-          placeholder="11/06/2022"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.waterDate}
-        />
-        {formik.touched.waterDate && formik.errors.waterDate ? (
-          <div className="error-msg">{formik.errors.waterDate}</div>
-        ) : null}
+              <label htmlFor="waterDate">Next Water Date:</label>
+              <Field
+                id="waterDate"
+                name="waterDate"
+                type="date"
+                placeholder="11/06/2022"
+              />
+              <ErrorMessage name="waterDate" component={TextError} />
 
-        <label htmlFor="feedDate">Next Feed Date:</label>
-        <input
-          id="feedDate"
-          name="feedDate"
-          type="date"
-          placeholder="11/01/2022"
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          value={formik.values.feedDate}
-        />
-        {formik.touched.feedDate && formik.errors.feedDate ? (
-          <div className="error-msg">{formik.errors.feedDate}</div>
-        ) : null}
+              <label htmlFor="feedDate">Next Feed Date:</label>
+              <Field
+                id="feedDate"
+                name="feedDate"
+                type="date"
+                placeholder="11/01/2022"
+              />
+              <ErrorMessage name="feedDate" component={TextError} />
 
-        {formik.status === "sent" ? (
-          <div className="success-msg">
-            <h3>Congrats on your new Plant Baby!</h3>
-            <Link to={`/plants`}>
-              <button type="button" className="cancel__btn secondary">
-                Back
-              </button>
-            </Link>
-          </div>
-        ) : (
-          <div>
-            <Link to={`/plants`}>
-              <button type="button" className="cancel__btn secondary">
-                Cancel
-              </button>
-            </Link>
-            <button type="submit" className="submit-new__btn secondary">
-              Submit
-            </button>
-          </div>
-        )}
-      </form>
+              {formik.status === "sent" ? (
+                <div className="success-msg">
+                  <h3>Congrats on your new Plant Baby!</h3>
+                  <button
+                    type="button"
+                    onClick={(e) => {
+                      /*prevent default submit behaviour which was causing validation errors even though I have type button*/
+                      e.preventDefault();
+                      formik.setStatus("");
+                    }}
+                    className="create-new__btn secondary"
+                  >
+                    Add Another
+                  </button>
+                  <Link to={`/plants`}>
+                    <button type="button" className="cancel__btn secondary">
+                      Back to List
+                    </button>
+                  </Link>
+                </div>
+              ) : (
+                <div>
+                  <Link to={`/plants`}>
+                    <button type="button" className="cancel__btn secondary">
+                      Cancel
+                    </button>
+                  </Link>
+                  <button type="submit" className="submit-new__btn secondary">
+                    Submit
+                  </button>
+                </div>
+              )}
+            </Form>
+          );
+        }}
+      </Formik>
     </section>
   );
 }
